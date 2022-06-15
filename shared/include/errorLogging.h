6a5bbd50b75468f9290874300cb1ec8eeb6cc46f
vlod <vladalex420@gmail.com>
2022-06-16 00:20:17 +0300

    added hex editor

---
+++ shared/include/errorLogging.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+
+struct ErrorLog
+{
+	char errorLog[256] = {};
+	
+	enum ErrorType : int
+	{
+		info = 0,
+		warn,
+		error,
+	};
+
+	int errorType = ErrorType::error;
+
+	void renderText();
+	void clearError();
+	void setError(const char* e, ErrorType type = ErrorType::error);
+};
+
+std::string getLastErrorString();
