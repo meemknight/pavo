f47a8aacaeb2fac1c69105bb762f3c6d97ad126b
vlod <vladalex420@gmail.com>
2022-06-16 11:07:15 +0300

    added again viewport

--- pavo-gui/src/platform/glfwMain.cpp
+++ pavo-gui/src/platform/glfwMain.cpp
@@ -331,9 +331,9 @@ int main()
 		//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
 		
-	#ifdef PAVO_UNIX
+	//#ifdef PAVO_UNIX
 		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
-	#endif
+	//#endif
 
 		//io.ConfigViewportsNoAutoMerge = true;
 		//io.ConfigViewportsNoTaskBarIcon = true;
