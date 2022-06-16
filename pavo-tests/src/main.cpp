eba4dcdcc5800e796eaae2c0262223cac41bed0f
vlod <vladalex420@gmail.com>
2022-06-16 10:55:16 +0300

    fixed tests

--- pavo-tests/src/main.cpp
+++ pavo-tests/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "virtualQuery.h"
+#include "errorLogging.h"
 
 #undef min
 #undef max
@@ -7,32 +8,7 @@
 #include <catch2/catch_template_test_macros.hpp>
 
 
-//todo move later
-//https://stackoverflow.com/questions/1387064/how-to-get-the-error-message-from-the-error-code-returned-by-getlasterror
-std::string getLastErrorString()
-{
-	//Get the error message ID, if any.
-	DWORD errorMessageID = ::GetLastError();
-	if (errorMessageID == 0)
-	{
-		return std::string(); //No error message has been recorded
-	}
 
-	LPSTR messageBuffer = nullptr;
-
-	//Ask Win32 to give us the string version of that message ID.
-	//The parameters we pass in, tell Win32 to create the buffer that holds the message for us (because we don't yet know how long the message string will be).
-	size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
-
-	//Copy the error message into a std::string.
-	std::string message(messageBuffer, size);
-
-	//Free the Win32's string's buffer.
-	LocalFree(messageBuffer);
-
-	return message;
-}
 
 TEST_CASE("Correct memory flags", "[memory parse test 1]") // 1
 {
